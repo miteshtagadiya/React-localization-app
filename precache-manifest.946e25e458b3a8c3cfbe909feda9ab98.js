6ef12340acaa333b6b9ac141b1c9862d8be5bc8b
mitesh <miteshtagadiya@gmail.com>
2020-03-29 17:46:56 +0530

    Updates

---
+++ precache-manifest.946e25e458b3a8c3cfbe909feda9ab98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603b201e67e8f0aa7d901255b0825d5c",
+    "url": "/React-localization-app/index.html"
+  },
+  {
+    "revision": "99cfb7aae45ce25146e6",
+    "url": "/React-localization-app/static/css/main.58bc66bc.chunk.css"
+  },
+  {
+    "revision": "f97802f58638df4bab28",
+    "url": "/React-localization-app/static/js/2.bde4366b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-localization-app/static/js/2.bde4366b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99cfb7aae45ce25146e6",
+    "url": "/React-localization-app/static/js/main.50ec346b.chunk.js"
+  },
+  {
+    "revision": "6247d26f09bd99bb88b8",
+    "url": "/React-localization-app/static/js/runtime-main.5749ef0c.js"
+  }
+]);
